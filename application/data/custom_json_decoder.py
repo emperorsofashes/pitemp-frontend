fe9e6f8734204d34f366ed30c174d854638ed9aa
Tyler <5478568+Tyler-Yates@users.noreply.github.com>
2024-02-11 16:32:22 -0600

    Data decimation

--- application/data/custom_json_decoder.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-from application.constants.app_constants import DATETIME_FORMAT_STRING
-
-
-def decode_json(obj):
-    if isinstance(obj, str) and ":" in obj and "-" in obj:
-        return datetime.datetime.strptime(obj, DATETIME_FORMAT_STRING)
